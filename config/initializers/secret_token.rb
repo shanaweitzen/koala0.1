5abd2e2fcb94a348de44d9291e78b07bde608e5f
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-27 12:14:58 -0700

    add rails 4 with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koala01::Application.config.secret_key_base = '42d746bc08a779a4ada221d6667f2ab7c7cc22f3138d9559de330b6f66815513229ac8990a53933dd35a904d8b0c4a120aae804f3db349e901107fb4eac0f447'
